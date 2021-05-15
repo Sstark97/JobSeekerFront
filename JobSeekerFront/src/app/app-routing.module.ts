497086ee3408b08d2cf64a9db29b7c302613f5d5
Sstark97 <aitorscinfo@gmail.com>
2021-05-15 10:27:14 +0100

    login/register-mockup

--- JobSeekerFront/src/app/app-routing.module.ts
+++ JobSeekerFront/src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CvComponent } from './components/cv/cv.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { OffersComponent } from './components/offers/offers.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: RegisterComponent,
+    // redirectTo: 'login',
+    // pathMatch: 'full'
   },
   {
     path: 'ofertas',
@@ -16,6 +20,14 @@ const routes: Routes = [
   {
     path: 'cv',
     component: CvComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 
--- JobSeekerFront/src/app/components/login/login.component.ts
+++ JobSeekerFront/src/app/components/login/login.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  offerts: any = [1,2,3,4,5];
   constructor() { }
 
   ngOnInit(): void {
